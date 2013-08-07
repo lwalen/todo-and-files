d6e44c4e257627b034eeb3b46873fbe4d72317c0
Lars Walen <lars@walen.me>
2013-08-07 10:21:13 -0600

    Added 404 page

---
+++ site/404.php
@@ -0,0 +1,15 @@
+<?php
+require_once "common.php";
+
+writeHead("404");
+?>
+
+
+<body>
+	<div class='section'>
+		<h1>404</h1>
+		<p>File not found.</p>
+		<a href="/">walen.me</a>
+	</div>
+<body>
+</html>
