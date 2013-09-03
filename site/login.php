50b35853cf2169d40b99f7171bf0b8eb656e57c0
Lars Walen <lars@walen.me>
2013-09-02 20:46:44 -0600

    Removed project links from homepage

--- site/login.php
+++ site/login.php
@@ -27,10 +27,6 @@ if(!function_exists('showLoginPasswordProtect')) {
 	<div class='section'>
 		<h1>walen.me</h1>
 		<a href='http://lars.walen.me/'>Lars Walen</a>
-		<span class='separator'> | </span>
-		<a href='//dailies.walen.me'>Dailies</a>
-		<span class='separator'> | </span>
-		<a href='//walen.me/artists_with_one_song'>Artists with one song</a>
 	</div>
 
 	<div class='section'>
